7c2605330669d9dcf1c762a8af71befe8205933c
Leo Vergara <leoivs@hotmail.com>
2022-09-07 22:53:20 -0300

    [Leo Vergara]-Creación repositorio Task

---
+++ voluntarios-be/src/main/java/com/app/voluntariosbe/repositories/TaskRepositoryImp.java
@@ -0,0 +1,11 @@
+package com.app.voluntariosbe.repositories;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class TaskRepositoryImp implements TaskRepository{
+
+    @Autowired
+    private
+}
