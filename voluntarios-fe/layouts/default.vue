b6c9d226826474415e05b34a21a6aeb94763aded
Leo Vergara <leoivs@hotmail.com>
2022-09-08 22:03:04 -0300

    [Leo Vergara]-Conexión frontend y backend

--- voluntarios-fe/layouts/default.vue
+++ voluntarios-fe/layouts/default.vue
@@ -106,6 +106,11 @@ export default {
           icon: 'mdi-chart-bubble',
           title: 'Inspire',
           to: '/inspire'
+        },
+        {
+          icon: 'mdi-file-document-check',
+          title: 'Tareas',
+          to: '/task'
         }
       ],
       miniVariant: false,
--- voluntarios-fe/pages/task/index.vue
+++ voluntarios-fe/pages/task/index.vue
@@ -1,30 +1,51 @@
 <template>
   <v-data-table
     :headers="headers"
-    :items="tarea"
-    :items-per-page="5"
+    :items="tareas"
+    :items-per-page="15"
     class="elevation-1"
   ></v-data-table>
 </template>
 
 <script>
-export default ({data()
-{  return {
-    headers: 
-    [
-      {
-        text: "Id",
-        align: "start",
-        sortable: false,
-        value: "id",
-      },
-      { text: "Nombre", value: "nombre" },
-      { text: "Descripción", value: "descrip" },
-      { text: "Voluntarios requeridos", value: "cant_vol_requeridos" },
-      { text: "Voluntarios inscritos", value: "cant_vol_inscritos" },
-      { text: "Id emergencia", value: "id_emergencia" },
-    ]
-  }
-}
+import axios from 'axios'
+export default ({
+  data(){
+    return {
+      headers:
+      [
+        {
+          text: "Id",
+          align: "start",
+          sortable: false,
+          value: "id",
+        },
+        { text: "Nombre", value: "nombre" },
+        { text: "Descripción", value: "descrip" },
+        { text: "Voluntarios requeridos", value: "cant_vol_requerido" },
+        { text: "Voluntarios inscritos", value: "cant_vol_inscritos" },
+        { text: "Fecha de inicio", value: "finicio" },
+        { text: "Fecha de fin", value: "ffin" },
+        { text: "Id estado", value: "id_estado" }
+      ],
+      tareas: []
+    }
+  },
+  mounted() {
+    this.getTask()
+  },
+  methods: {
+    async getTask(){
+      const url = 'http://localhost:8080/tasks'
+      await axios.get(url)
+      .then(response => {
+        console.log(response.data)
+        this.tareas = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  },
 })
 </script>
